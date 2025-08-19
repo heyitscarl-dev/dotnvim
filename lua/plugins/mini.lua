b099ab7027227777cdbae81f883f0a08d9c6ffb5
heyitscarl <carl.nw.neben@gmail.com>
2025-08-20 01:19:41 +0200

    feat: add mini.hipatterns plugin
    add mini.hipatterns setup function to mini.nvim plugin configuration (in
    config function)

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,6 +10,14 @@ return {
             require("mini.splitjoin").setup()
             require("mini.surround").setup()
             require("mini.statusline").setup()
+            require("mini.hipatterns").setup({
+                highlighters = {
+                    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+                    hack  = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+                    todo  = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+                    note  = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+                }
+            })
         end
     },
     {
